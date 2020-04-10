bf7d6c953e79b9356fae36f0ca1fa4065193bf13
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-04-10 16:25:01 +1000

    quiz game start

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './bootstrap.min.css'
+
+class AuthorQuiz extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Author Quiz</p>
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
